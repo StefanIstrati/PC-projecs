8bd86d110f8aafc7c4475834bcf115fb47c6e5c7
StefanIstrati <145895345+StefanIstrati@users.noreply.github.com>
2023-09-24 11:07:07 +0300

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 void Sort(int arr[], int n) {
     int i, j, min, k;
     for (i = 0; i < n - 1; i++) {
@@ -16,7 +15,6 @@ void Sort(int arr[], int n) {
         arr[min] = k;
     }
 }
-
 void fillMatrix(int **arr2, int N, int M, int arr[], int row, int col, int k) {
     if (N <= 0 || M <= 0) return;
     
@@ -24,22 +22,19 @@ void fillMatrix(int **arr2, int N, int M, int arr[], int row, int col, int k) {
     for (int i = col; i < col + M; i++) {
         arr2[row][i] = arr[k];
         k++;
-    }
-    
+    } 
     // Fill the right column
     for (int i = row + 1; i < row + N; i++) {
         arr2[i][col + M - 1] = arr[k];
         k++;
-    }
-    
+    } 
     // Fill the bottom row
     if (N > 1) {
         for (int i = col + M - 2; i >= col; i--) {
             arr2[row + N - 1][i] = arr[k];
             k++;
         }
-    }
-    
+    } 
     // Fill the left column
     if (M > 1) {
         for (int i = row + N - 2; i > row; i--) {
@@ -47,11 +42,9 @@ void fillMatrix(int **arr2, int N, int M, int arr[], int row, int col, int k) {
             k++;
         }
     }
-    
     // Recursively fill the inner matrix
     fillMatrix(arr2, N - 2, M - 2, arr, row + 1, col + 1, k);
 }
-
 void printMatrix(int **matrix, int N, int M) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
@@ -61,7 +54,6 @@ void printMatrix(int **matrix, int N, int M) {
         printf("\n");
     }
 }
-
 int main() {
     int N, M;
     printf("Enter the number of rows (N) and columns (M) of the matrix: ");
@@ -71,33 +63,26 @@ int main() {
     for (int i = 0; i < N; i++) {
         arr2[i] = (int *)malloc(M * sizeof(int));
     }
-    
     int arr[N * M];
     int k;
-
     k = N * M;
     srand(time(NULL));
     for (int i = 0; i < k; i++) {
         arr[i] = (rand() % 100);
     }
-    
     k = 0;
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < M; j++) {
             arr2[i][j] = arr[k];
             k++;
         }
-    }
-    
+    } 
     printf("Initial bidimensional array:\n");
-    printMatrix(arr2, N, M);
-    
+    printMatrix(arr2, N, M); 
     Sort(arr, k);
     k = 0;
     fillMatrix(arr2, N, M, arr, 0, 0, k);
-
     printf("Final bidimensional array sorted (in the center the highest value and towards the edges the lowest value):\n");
     printMatrix(arr2, N, M);
-
     return 0;
 }
